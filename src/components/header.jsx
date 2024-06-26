3ff3c5c875f0b8886dd02d31af63faad9265275b
Gurpreet Singh <117374055+Gurpreet455@users.noreply.github.com>
2024-06-26 13:05:21 +0530

    first commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,29 +3,29 @@ import React from 'react'
 export default function header() {
   return (
     <nav>
-          <div class="container">
-            <div class="navbar">
-              <div class="nav-logo"><img src="" alt="" /></div>
-                <ul class="nav-links">
-                    <li><a href="#" class="active">Home</a></li>
+          <div className="container">
+            <div className="navbar">
+              <div className="nav-logo"><img src="" alt="" /></div>
+                <ul className="nav-links">
+                    <li><a href="#" className="active">Home</a></li>
                     <li><a href="#Aout">My Jobs</a></li>
-                    <li><a href="#">user</a></li>
+                    <li><a href="#"><i className="fa-regular fa-bell"></i></a></li>
                 </ul>
-                <ul class="nav-links">
-                    <li><a href="$" class="active">For Employers</a></li>
+                <ul className="nav-links">
+                    <li><a href="$" className="active">For Employers</a></li>
                  
                 </ul>
-                <div class="info-box">
-                  <div class="icons">
-                    <span><i class="fa-solid fa-heart"></i></span>
-                    <span><i class="fa-solid fa-cart-shopping"></i></span>
-                    <span><i class="fa-solid fa-user"></i></span>
+                <div className="info-box">
+                  <div className="icons">
+                    <span><i className="fa-solid fa-heart"></i></span>
+                    <span><i className="fa-solid fa-cart-shopping"></i></span>
+                    <span><i className="fa-solid fa-user"></i></span>
                   </div>
                 </div>
-          <div class="burger" onclick="toggleMenu()">
-              <div class="line"></div>
-              <div class="line"></div>
-              <div class="line"></div>
+          <div className="burger" onclick="toggleMenu()">
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
           </div>
             </div>
           </div>
